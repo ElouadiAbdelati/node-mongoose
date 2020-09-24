1a137b2fbf81bd1c6bb90f4c45b7e0a6f1e5fbdd
ElouadiAbdelati <57566321+ElouadiAbdelati@users.noreply.github.com>
2020-09-24 15:00:53 +0200

    Mongoose Part 3

--- index.js
+++ index.js
@@ -8,18 +8,33 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
 
     console.log('Connected correctly to server');
-
     Dishes.create({
-            name: 'Uthapizza',
-            description: 'Test'
+            name: 'Uthappizza',
+            description: 'test'
         })
         .then((dish) => {
             console.log(dish);
 
-            return Dishes.find({}).exec();
+            return Dishes.findByIdAndUpdate(dish._id, {
+                    $set: { description: 'Updated test' }
+                }, {
+                    new: true
+                })
+                .exec();
         })
-        .then((dishes) => {
-            console.log(dishes);
+        .then((dish) => {
+            console.log(dish);
+
+            dish.comments.push({
+                rating: 5,
+                comment: 'I\'m getting a sinking feeling!',
+                author: 'Leonardo di Carpaccio'
+            });
+
+            return dish.save();
+        })
+        .then((dish) => {
+            console.log(dish);
 
             return Dishes.remove({});
         })
@@ -29,5 +44,6 @@ connect.then((db) => {
         .catch((err) => {
             console.log(err);
         });
+
 
 });
